b24d164a5b776ce06e5987fdb0d199cd264147c0
Arthurdff <56809264+Arthurdff@users.noreply.github.com>
2022-04-25 14:30:07 -0300

    Add files via upload

---
+++ src/main/java/com/netfujix/resource/PlanoResource.java
@@ -0,0 +1,38 @@
+package com.netfujix.resource;
+
+import com.netfujix.model.Plano;
+import com.netfujix.service.PlanoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/plano")
+public class PlanoResource {
+
+    @Autowired
+    private PlanoService service;
+
+    @PostMapping
+    public Plano salvar(@RequestBody Plano plano) {
+        return service.salvar(plano);
+    }
+
+    @PutMapping
+    public Plano atualizar(@RequestBody Plano plano) throws Exception {
+        return service.atualizar(plano);
+    }
+
+    @GetMapping
+    public List<Plano> listar() {
+        return service.listar();
+    }
+
+    @DeleteMapping("{id}")
+    public void remover(@PathVariable int id) {
+        service.deletar(id);
+    }
+
+}
+
