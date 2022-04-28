cc8861f19807ed85daab003b157fd40c858997a5
Arthurdff <56809264+Arthurdff@users.noreply.github.com>
2022-04-28 20:01:05 -0300

    Update PlanoRepository.java

--- src/main/java/com/netfujix/repository/PlanoRepository.java
+++ src/main/java/com/netfujix/repository/PlanoRepository.java
@@ -1,10 +1,21 @@
 package com.netfujix.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.netfujix.model.Plano;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface PlanoRepository extends JpaRepository<Plano, Integer> {
+    Optional<Plano> findById(Integer id);
+
+    @Query("select f from Usuario f where f.usuario.nome = :usuario")
+    List<Plano> buscaByUsuario(@Param("usuario") String usuario);
+
 
 }
