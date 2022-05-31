b8dc47732f5595d18e504c917ce13d39f55d3fe0
Diego Herculano <diegoherculanojp@gmail.com>
2022-05-31 18:54:20 -0300

    fix: main hash password

--- src/main/java/com/netfujix/resource/FilmeResource.java
+++ src/main/java/com/netfujix/resource/FilmeResource.java
@@ -7,6 +7,7 @@ import com.netfujix.model.Filme;
 import com.netfujix.service.FilmeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/filme")
 public class FilmeResource {
 
